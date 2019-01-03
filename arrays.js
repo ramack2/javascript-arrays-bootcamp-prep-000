11f5b43e96e7335e90451d5dced90cefb0bb5e7e
Kriselle Ramac <krisellaella@gmail.com>
2019-01-03 04:41:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 //define two functions: addElementToBeginningOfArray, destructivelyAddElementToBegininningOfArray
 
+//define two functions: addElementToEndOfArray, destructivelyAddElementToEndOfArray
+
 //define a function called accessElementInArray. The function shold accept an array and an index and return the element at that index.
 
 //define a function called removeElementFromBeginningOfArray. The function should remove the first element in the array. This function should return the entire array in the same line, and it should not mutate the original array.
