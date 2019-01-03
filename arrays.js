9afb611d19b4a0a40856eca93879fa28ece18976
Kriselle Ramac <krisellaella@gmail.com>
2019-01-03 04:35:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 //define two functions: addElementToBeginningOfArray, destructivelyAddElementToBegininningOfArray
+
+//define a function called accessElementInArray. The function shold accept an array and an index and return the element at that index.
