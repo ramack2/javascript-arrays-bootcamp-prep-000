cd9edb5556e2799483156decaf1d0cc1da3b3e37
Kriselle Ramac <krisellaella@gmail.com>
2019-01-03 04:30:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,4 @@
-## define a 
+//define a variable called chocolateBars. Its value should be an array of the strings snickers, hundred grand, kitkat, and skittles
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
